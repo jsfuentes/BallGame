13bfc4e348b45b2516554e8f9782f5ebb8000fe5
Jorge Fuentes <jsjfuentesj@yahoo.com>
2017-04-10 15:21:46 -0700

    Add lives and score and gameover

--- app/src/main/java/com/example/student/gameapp/Balloon.java
+++ app/src/main/java/com/example/student/gameapp/Balloon.java
@@ -1,10 +1,99 @@
 package com.example.student.gameapp;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
+import com.example.student.gameapp.utils.PixelHelper;
+
 /**
- * Created by Student on 4/10/2017.
+ * Created by Student on 4/9/2017.
  */
 
-public class Balloon extends ImageView {
+public class Balloon extends android.support.v7.widget.AppCompatImageView implements Animator.AnimatorListener, ValueAnimator.AnimatorUpdateListener {
+
+    private ValueAnimator mAnimator;
+    private BalloonListener mListener;
+    private boolean mPopped;
+
+    public Balloon(Context context) {
+        super(context);
+    }
+
+    public Balloon(Context context, int color, int rawSize) {
+        super(context);
+
+        mListener = (BalloonListener) context;
+
+        this.setImageResource(R.drawable.ball);
+        this.setColorFilter(color);
+
+        int dpHeight = PixelHelper.pixelsToDp(rawSize, context);
+        int dpWidth = PixelHelper.pixelsToDp(rawSize, context);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(dpWidth, dpHeight);
+        setLayoutParams(params);
+    }
+
+    public void releaseBalloon(int screenHeight, int duration){
+        mAnimator = new ValueAnimator();
+        mAnimator.setDuration(duration);
+        mAnimator.setFloatValues(screenHeight, 0f);
+        mAnimator.setInterpolator(new LinearInterpolator());
+        mAnimator.setTarget(this);
+        mAnimator.addListener(this);
+        mAnimator.addUpdateListener(this);
+        mAnimator.start();
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        if (!mPopped) {
+            mListener.popBalloon(this, false);
+        }
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator animation) {
+        setY((Float) animation.getAnimatedValue());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!mPopped && event.getAction() == MotionEvent.ACTION_DOWN) {
+            mListener.popBalloon(this, true);
+            mPopped = true;
+            mAnimator.cancel();
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void setPopped(boolean popped) {
+        mPopped = popped;
+        if(popped){
+            mAnimator.cancel();
+        }
+    }
+
+    public interface BalloonListener{
+        void popBalloon(Balloon balloon, boolean userTouch);
+    }
 }
--- app/src/main/java/com/example/student/gameapp/MainActivity.java
+++ app/src/main/java/com/example/student/gameapp/MainActivity.java
@@ -1,28 +1,51 @@
 package com.example.student.gameapp;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
-    private int[] mBallonColors = new int[3];
+public class MainActivity extends AppCompatActivity 
+    implements Balloon.BalloonListener{
+
+    private static final int MIN_ANIMATION_DELAY = 500;
+    private static final int MAX_ANIMATION_DELAY = 1500;
+    private static final int MIN_ANIMATION_DURATION = 1000;
+    private static final int MAX_ANIMATION_DURATION = 8000;
+    private static final int NUMBER_OF_LIVES = 4;
+    private static final int[] LIFECYCLE = new int[] {R.drawable.ic_signal_wifi_4_bar_black_24dp,
+            R.drawable.ic_signal_wifi_3_bar_black_24dp, R.drawable.ic_signal_wifi_2_bar_black_24dp,
+            R.drawable.ic_signal_wifi_1_bar_black_24dp, R.drawable.ic_signal_wifi_0_bar_black_24dp};
+
+    private int[] mBalloonColors = new int[3];
     private int mNextColor, mScreenWidth, mScreenHeight;
-
+    private int mLevel, mScore, mLivesLost;
     private ViewGroup mContentView;
+    TextView mScoreDisplay, mLevelDisplay;
+    private ImageView mLifeBar;
+    private List<Balloon> mBalloons = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBallonColors[0] = Color.argb(255, 255, 0, 0);
-        mBallonColors[1] = Color.argb(255, 0, 255, 0);
-        mBallonColors[2] = Color.argb(255, 0, 0, 255);
+        mBalloonColors[0] = Color.argb(255, 255, 0, 0);
+        mBalloonColors[1] = Color.argb(255, 0, 255, 0);
+        mBalloonColors[2] = Color.argb(255, 0, 0, 255);
 
         getWindow().setBackgroundDrawableResource(R.drawable.sg);
 
@@ -48,25 +71,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mContentView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    Ballon b = new Ballon(MainActivity.this, mBallonColors[mNextColor], 100);
-                    b.setX(event.getX());
-                    b.setY(mScreenHeight);
-                    mContentView.addView(b);
-                    b.releaseBallon(mScreenHeight, 3000);
+        mScoreDisplay = (TextView) findViewById(R.id.score_display);
+        mLevelDisplay = (TextView) findViewById(R.id.level_display);
+        mLifeBar = (ImageView) findViewById(R.id.imageView);
+        mLifeBar.setImageResource(LIFECYCLE[mLivesLost]);
+        updateDisplay();
 
-                    if (mNextColor + 1 == mBallonColors.length) {
-                        mNextColor = 0;
-                    } else {
-                        mNextColor += 1;
-                    }
-                }
-                return false;
-            }
-        });
+//        mContentView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    Balloon b = new Balloon(MainActivity.this, mBalloonColors[mNextColor], 40);
+//                    b.setX(event.getX());
+//                    b.setY(mScreenHeight);
+//                    mContentView.addView(b);
+//                    b.releaseBalloon(mScreenHeight, 3000);
+//
+//                    if (mNextColor + 1 == mBalloonColors.length) {
+//                        mNextColor = 0;
+//                    } else {
+//                        mNextColor += 1;
+//                    }
+//                }
+//                return false;
+//            }
+//        });
     }
 
     private void setToFullScreen(){
@@ -83,5 +112,122 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         setToFullScreen();
+    }
+
+    private void startLevel(){
+        mLevel++;
+        updateDisplay();
+        BalloonLauncher launcher = new BalloonLauncher();
+        launcher.execute(mLevel);
+    }
+
+    public void goButtonClickHandler(View view) {
+        startLevel();
+    }
+
+    @Override
+    public void popBalloon(Balloon balloon, boolean userTouch) {
+        mContentView.removeView(balloon);
+        mBalloons.remove(balloon);
+
+        if (userTouch) {
+            mScore++;
+        }
+        else {
+            mLivesLost++;
+            if(mLivesLost <= NUMBER_OF_LIVES) {
+                mLifeBar.setImageResource(LIFECYCLE[mLivesLost]);
+            }
+            if(mLivesLost == NUMBER_OF_LIVES) {
+                gameOver(true);
+                return;
+            } else {
+                Toast.makeText(this, "Missed that one!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        updateDisplay();
+    }
+
+    private void gameOver(boolean b) {
+        Toast.makeText(this, "GAME OVER!", Toast.LENGTH_SHORT).show();
+        for (Balloon balloon : mBalloons) {
+            mContentView.removeView(balloon);
+            balloon.setPopped(true);
+        }
+        mBalloons.clear();
+
+    }
+
+    private void updateDisplay() {
+        mScoreDisplay.setText(String.valueOf(mScore));
+        mLevelDisplay.setText(String.valueOf(mLevel));
+    }
+
+    private class BalloonLauncher extends AsyncTask<Integer, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+
+            if (params.length != 1) {
+                throw new AssertionError(
+                        "Expected 1 param for current level");
+            }
+
+            int level = params[0];
+            int maxDelay = Math.max(MIN_ANIMATION_DELAY,
+                    (MAX_ANIMATION_DELAY - ((level - 1) * 500)));
+            int minDelay = maxDelay / 2;
+
+            int balloonsLaunched = 0;
+            while (balloonsLaunched < 3) {
+
+//              Get a random horizontal position for the next balloon
+                Random random = new Random(new Date().getTime());
+                int xPosition = random.nextInt(mScreenWidth - 145);
+                publishProgress(xPosition);
+                balloonsLaunched++;
+
+//              Wait a random number of milliseconds before looping
+                int delay = random.nextInt(minDelay) + minDelay;
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            int xPosition = values[0];
+            launchBalloon(xPosition);
+        }
+
+    }
+
+    private void launchBalloon(int x) {
+
+        Balloon balloon = new Balloon(this, mBalloonColors[mNextColor], 40);
+        mBalloons.add(balloon);
+
+        if (mNextColor + 1 == mBalloonColors.length) {
+            mNextColor = 0;
+        } else {
+            mNextColor++;
+        }
+
+//      Set balloon vertical position and dimensions, add to container
+        balloon.setX(x);
+        balloon.setY(mScreenHeight + balloon.getHeight());
+        mContentView.addView(balloon);
+
+//      Let 'er fly
+        int duration = Math.max(MIN_ANIMATION_DURATION, MAX_ANIMATION_DURATION - (mLevel * 1000));
+        balloon.releaseBalloon(mScreenHeight, duration);
+
     }
 }
